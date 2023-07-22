d9d038a25d018aff4182cd3e5815f06950d5abda
NehaRaut4 <sneharaut8792@gmail.com>
2023-07-22 09:38:15 +0530

    Using finalize(),System.gc(), System.runFinalization() achieved desired o/p

--- Employee.java
+++ Employee.java
@@ -4,13 +4,12 @@ public class Employee {
 	private int id;
 	private String name;
 	private int salary;
-	private static int nextId=1; //static-shared among all objects
-	//Parameterized constructor
+	private static int nextId=1;
 	public Employee(String name, int salary) {
 		super();
 		this.name = name;
 		this.salary = salary;
-		this.id = nextId++;//to autogenerate employee id
+		this.id = nextId++;
 	}
 	//method to display employee details
 	public void show()
@@ -22,5 +21,11 @@ public class Employee {
 	{
 		System.out.println("Next employee Id will be - "+nextId);
 	}
-	
+	//overriding finalize() method
+	public void finalize()
+	{
+		--nextId;
+		//gc will call finalize()
+		//for two times for two objects e4 and e5
+	}
 }
--- Garbage_collector.java
+++ Garbage_collector.java
@@ -2,7 +2,7 @@ package com.app;
 
 public class Garbage_collector {
 	public static void main(String[] args) {
-		//without finalize() method
+		
 		Employee e1 = new Employee("sneha",100000);
 		Employee e2 = new Employee("Kunal",578589);
 		Employee e3 = new Employee("Priya",7989877);
@@ -13,15 +13,36 @@ public class Garbage_collector {
 		e1.showNextId();
 		e2.showNextId();
 		e3.showNextId();
+		//subblock 
+		{
+			Employee e4 = new Employee("Komal",132200000);
+			Employee e5 = new Employee("Sayali",97465600);
+			e4.show();
+			e5.show();
+			e4.showNextId();
+			e5.showNextId();
+			//nullifying objects so GC can delete them
+			e4=e5=null; 
+			System.gc();
+			System.runFinalization();
+		}
+		e1.showNextId();
+		
 		
 	}
 }
-//here not showing o/p properly
-/* Output
-ID - 1 Name - sneha Salary - 100000
+/*
+ * Correct OUTPUT with using finalize(),System.gc(),System.runFinalization()
+ ID - 1 Name - sneha Salary - 100000
 ID - 2 Name - Kunal Salary - 578589
 ID - 3 Name - Priya Salary - 7989877
 Next employee Id will be - 4
 Next employee Id will be - 4
 Next employee Id will be - 4
-*/
+ID - 4 Name - Komal Salary - 132200000
+ID - 5 Name - Sayali Salary - 97465600
+Next employee Id will be - 6
+Next employee Id will be - 6
+Next employee Id will be - 4
+
+ */
